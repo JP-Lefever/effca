0b340119d21974680e839a7ad501dbdce286c98e
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-06-04 13:44:56 +0200

    feat: add menu nav

--- src/components/navigation/NavAdmin/NavAdmin.tsx
+++ src/components/navigation/NavAdmin/NavAdmin.tsx
@@ -1,6 +1,43 @@
+"use client"
+import styles from "./navAdmin.module.css"
+import dataNav from "@/assets/data/navBar/navAdmin.json"
+import {useState} from "react";
+import Link from "next/link";
+
+
 export default function NavAdmin() {
 
+    const [openSection, setOpenSection] = useState<Partial<Record<keyof typeof dataNav, boolean>>>({})
+
+    const toggleSection = (key : keyof typeof dataNav) => {
+      setOpenSection((prev)=>({
+          ...prev,
+          [key] : !prev[key],
+      }))
+
+    }
+
     return (
-        <></>
+        <nav className={styles.nav}>
+            <ul>
+                {Object.entries(dataNav).map(([key,value])=>{
+                    const typeKey = key as keyof typeof dataNav;
+                    return (
+                        <li key={key}>
+                            <button className={styles.button} onClick={()=>toggleSection(typeKey)} type={"button"}>{value.title}</button>
+                            {openSection[typeKey] &&
+                            <ul>
+                                {value.items.map((item, i)=>(
+                                    <Link href={"#"} className={styles.menu} key={i}>
+                                        {item}
+                                    </Link>
+                                ))}
+                            </ul>
+                            }
+                        </li>
+                    )
+                })}
+            </ul>
+        </nav>
     )
 }
