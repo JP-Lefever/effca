eb1c80f72eea8e8f759721a92ac6ce7fc437021e
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-06-16 16:18:45 +0200

    feat: add styles

--- src/features/actuality/components/cardActuality/CardActuality.tsx
+++ src/features/actuality/components/cardActuality/CardActuality.tsx
@@ -1,7 +1,7 @@
 import styles from "./cardActuality.module.css"
 import Image from 'next/image';
 import {ActualityProps} from "@/features/actuality/type";
-import {act} from "react";
+
 
 export default function CardActuality({actuality} : {actuality: ActualityProps }) {
 
