892a4913c9fe9a838eb57cc3e98f8c4948fda220
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-06-17 12:43:54 +0200

    feat: add styles

--- src/features/actuality/components/detailActuality/DetailActuality.tsx
+++ src/features/actuality/components/detailActuality/DetailActuality.tsx
@@ -3,7 +3,8 @@ import styles from "./detailActuality.module.css"
 import {ActualityProps} from "@/features/actuality/type";
 import Image from "next/image"
 import {useRouter} from "next/navigation";
-import {X, SquareArrowUp } from "lucide-react"
+import {X, SquareArrowUp,User, Phone, AtSign } from "lucide-react"
+import {formatedDate} from "@/assets/helpers/formatedDate";
 
 export default function DetailActuality({actuality }: {actuality: ActualityProps}) {
 
@@ -14,18 +15,25 @@ export default function DetailActuality({actuality }: {actuality: ActualityProps
         <section id={"top"} className={styles.section}>
 
                 <button className={styles.button} type={"button"} onClick={() => {router.push("/actuality")}}><X size={48} color={"black"}/></button>
-
-                <Image className={styles.image} src={actuality.photo as string} alt={actuality.title} width = {860} height = {640} />
+                <article>
+                    <h2 className={styles.h2head}>{actuality.title}</h2>
+                    <Image className={styles.image} src={actuality.photo as string} alt={actuality.title} width = {860} height = {640} />
+                </article>
             <article>
-                <h2>{actuality.title}</h2>
+                <h2 className={styles.h2}>{actuality.title}</h2>
+                <h3 className={styles.h3}>{formatedDate(actuality.date)}</h3>
                 <p className={styles.p}>{actuality.description}</p>
             </article>
+            {actuality.contact &&
             <article>
-                <h3>Contact :</h3>
-                <p className={styles.p}>{actuality.contact}</p>
-                <p className={styles.p}>{actuality.phone}</p>
-                <p className={styles.p}>{actuality.mail}</p>
+                <h3 className={styles.h3contact}>Demande de renseignement :</h3>
+                <ul>
+                    <li className={styles.pList}><User/> : {actuality.contact}</li>
+                    <li className={styles.pList}><Phone/> : {actuality.phone}</li>
+                    <li className={styles.pList}><AtSign/> : {actuality.mail}</li>
+                </ul>
             </article>
+            }
             <a href={"#top"} className={styles.anchor}>
                 <SquareArrowUp size={48} color={"black"}/>
             </a>
