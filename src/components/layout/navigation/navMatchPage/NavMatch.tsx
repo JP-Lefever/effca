3b63804b6c9861e0c50161e16cad2b92739ced75
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-06-19 12:19:42 +0200

    feat add styles

--- src/components/layout/navigation/navMatchPage/NavMatch.tsx
+++ src/components/layout/navigation/navMatchPage/NavMatch.tsx
@@ -1,17 +1,21 @@
+"use client"
 import styles from "./navMatch.module.css"
 import Link from "next/link";
 import data from "@/assets/data/matchPage/match.json"
+import clsx from "clsx";
+import {usePathname} from "next/navigation";
 
 export default function NavMatch() {
 
-
+    const pathname = usePathname()
+    console.log("pathname:", JSON.stringify(pathname));
     return (
         <>
             <nav className={styles.nav}>
                 <ul className={styles.ul}>
                     {data.navBar.map((item) => (
                         <li className={styles.li} key={item.id}>
-                            <Link className={styles.link} href={item.href}>{item.title}</Link>
+                            <Link className={clsx(styles.link,{[styles.isActiveLink] : pathname === item.href })} href={item.href}>{item.title}</Link>
                         </li>
                     ))}
 
