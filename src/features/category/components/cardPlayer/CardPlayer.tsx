54df1b55d284df1a17df89f5b012f2e2f6d979f9
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-06-20 14:41:36 +0200

    feat: add components players & cardPayer

---
+++ src/features/category/components/cardPlayer/CardPlayer.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+
+export default function CardPlayer(){
+
+
+    return (
+        <>
+        <section>
+            <figure>
+                <Image src={""} alt={""} width={220} height={220} />
+            </figure>
+            <article>
+                <h2></h2>
+                <h2></h2>
+            </article>
+        </section>
+
+        </>
+    )
+}
