b7f72696ffea063423f4f7bc338b93c6a931863f
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-06-23 14:26:10 +0200

    feat: resize image

--- next.config.ts
+++ next.config.ts
@@ -15,9 +15,9 @@ const nextConfig: NextConfig = {
       ],
   }
 };
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-});
-module.exports = withBundleAnalyzer({});
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//     enabled: process.env.ANALYZE === 'true',
+// });
+// module.exports = withBundleAnalyzer({});
 
 export default nextConfig;
