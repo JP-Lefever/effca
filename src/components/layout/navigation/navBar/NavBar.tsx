781fdde85b683c39caf2cfb1ea0ce5e3a4e1102f
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-06-25 11:45:34 +0200

    fix: styles & responsive nav

--- src/components/layout/navigation/navBar/NavBar.tsx
+++ src/components/layout/navigation/navBar/NavBar.tsx
@@ -8,55 +8,55 @@ import {useState} from "react";
 
 export default function NavBar(){
 
-    const [openMenu, setOpenMenu] = useState(true)
+    const [openMenu, setOpenMenu] = useState(false)
+
 
     const toggleMenu = () => {
-        setOpenMenu(!openMenu);
+        setOpenMenu(false);
     }
 
     return (<>
-    <nav className={openMenu ? styles.navOpen : styles.nav}>
-        <button className={styles.burgerMenu}  type={"button"} onClick={toggleMenu}>
+    <nav className={styles.nav}>
+        <button className={styles.burgerButton}  type={"button"} onClick={()=>setOpenMenu(!openMenu)}>
             {openMenu ? <X color={"white"}/> : <AlignJustify color={"white"}/>}
         </button>
-        {openMenu && (
-        <ul className={styles.ul}>
+
+        <ul className={`${styles.navList} ${openMenu ? styles.navMobileOpen : ""} `}>
           <li>
-              <Link className={styles.link} href={dataNav.home.link}>{dataNav.home.title}</Link>
+              <Link className={styles.link} onClick={toggleMenu} href={dataNav.home.link}>{dataNav.home.title}</Link>
           </li>
             <li>
-                <Link className={styles.link} href={dataNav.club.link}>{dataNav.club.title}</Link>
+                <Link className={styles.link} onClick={toggleMenu} href={dataNav.club.link}>{dataNav.club.title}</Link>
             </li>
             <li className={styles.dropDownMenu}>
-                <div className={styles.dropDownMenuTrigger}>
+                <p className={styles.dropDownMenuTrigger}>
                 {dataNav.team.title} <span>&#9660;</span>
-                </div>
+                </p>
                 <ul className={styles.dropDown}>
                         {dataNav.team.items.map((item, index) => (
-                    <li key={index}>
-                            <Link className={styles.subLink} href={dataNav.team.link[index]}>{item}</Link>
+                    <li className={styles.liDropdown} key={index}>
+                            <Link className={styles.subLink} onClick={toggleMenu} href={dataNav.team.link[index]}>{item}</Link>
                     </li>
                         ))}
                 </ul>
             </li>
             <li className={styles.imageContainer} >
-                <Link className={styles.link} href={dataNav.logo.link}>
+                <Link className={styles.link} onClick={toggleMenu} href={dataNav.logo.link}>
                     <Image className={styles.image} src={dataNav.logo.title} alt={"Logo club"} priority width={200} height={200}/>
                 </Link>
             </li>
             <li>
-                <Link className={styles.link} href={dataNav.match.link}>{dataNav.match.title}</Link>
+                <Link className={styles.link} onClick={toggleMenu} href={dataNav.match.link}>{dataNav.match.title}</Link>
             </li>
             <li>
-                <Link className={styles.link} href={dataNav.actuality.link}>{dataNav.actuality.title}</Link>
+                <Link className={styles.link} onClick={toggleMenu} href={dataNav.actuality.link}>{dataNav.actuality.title}</Link>
             </li>
             <li>
-                <Link className={styles.link} href={dataNav.partner.link}>{dataNav.partner.title}</Link>
+                <Link className={styles.link} onClick={toggleMenu} href={dataNav.partner.link}>{dataNav.partner.title}</Link>
             </li>
-
 
         </ul>
-        )}
+
     </nav>
     </>)
 }
