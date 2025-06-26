73f85bd3f3f2150cf6587103e4d2544bf2c9286a
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-06-26 13:02:06 +0200

    feat: add responsive

--- src/features/actuality/components/actualityListAdmin/ActualityList.tsx
+++ src/features/actuality/components/actualityListAdmin/ActualityList.tsx
@@ -18,32 +18,32 @@ export default function ActualityList({actualities} : {actualities: ActualityPro
 
     return(<>
 
-        <table className={styles.table}>
-            <caption>
-                Liste des évènements
-            </caption>
-            <thead>
-            <tr>
-                <th>Intitulé de l'évènement</th>
-                <th>Date de l'évènement</th>
-                <th>Modifier</th>
-                <th>Supprimer</th>
-            </tr>
-            </thead>
-        {actualities.map((actuality)=>(
-            <tbody key={actuality.id}>
+            <table className={styles.table}>
+                <caption>
+                    Liste des évènements
+                </caption>
+                <thead>
                 <tr>
-                    <td>{actuality.title}</td>
-                    <td>{formatedDate(actuality.date)}</td>
-                    <td><Link className={styles.link} href={`/admin/modifyActuality/${actuality.id}`}><FilePenLine/></Link></td>
-                    <td><button onClick={handleOpenModal} className={styles.link} type={"button"}><Trash2/></button></td>
+                    <th>Intitulé de l'évènement</th>
+                    <th>Date de l'évènement</th>
+                    <th>Modifier</th>
+                    <th>Supprimer</th>
                 </tr>
-        {openModal && (
-            createPortal(<ModalDeleteActuality id={actuality.id} closeModalAction={handleOpenModal}/>, document.body)
-        )}
-            </tbody>
-        ))}
-        </table>
+                </thead>
+            {actualities.map((actuality)=>(
+                <tbody key={actuality.id}>
+                    <tr>
+                        <td>{actuality.title}</td>
+                        <td>{formatedDate(actuality.date)}</td>
+                        <td><Link className={styles.link} href={`/admin/modifyActuality/${actuality.id}`}><FilePenLine/></Link></td>
+                        <td><button onClick={handleOpenModal} className={styles.link} type={"button"}><Trash2/></button></td>
+                    </tr>
+            {openModal && (
+                createPortal(<ModalDeleteActuality id={actuality.id} closeModalAction={handleOpenModal}/>, document.body)
+            )}
+                </tbody>
+            ))}
+            </table>
 
     </>)
 }
