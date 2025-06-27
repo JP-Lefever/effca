94109fd5cab89dc929d26515183e03ac5dec6aa6
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-06-27 11:01:01 +0200

    feat: add component not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import styles from "./not-found.module.css"
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NotFound(){
+
+
+    return (
+        <>
+            <section className={styles.section}>
+                <article className={styles.article}>
+                    <h1 className={styles.h1}>404 - Page Non trouvée</h1>
+                    <Link className={styles.link} href={"/"}>{"Cliquez ici pour revenir à l'accueil"}</Link>
+                    <Link href={"/"}><Image src={"/images/logo.webp"} alt="logo" height={480} width={480} /></Link>
+                </article>
+            </section>
+        </>
+    )
+}
