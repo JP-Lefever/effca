7cb56a25b2692232f7e16bf62dc3a5ef0eb612ba
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-07-29 12:29:24 +0200

    fix: clean code unused

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -4,7 +4,7 @@ import NavBar from "@/components/layout/navigation/navBar/NavBar";
 import Footer from "@/components/layout/footer/Footer";
 import {readAllCategories} from "@/features/team/action";
 import {notFound} from "next/navigation";
-import {Metadata} from "next";
+
 
 
 
