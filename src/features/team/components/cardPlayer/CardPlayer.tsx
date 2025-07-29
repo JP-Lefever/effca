f8c786e5db832d021841fc9dbc7f61d5e14972a0
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-07-29 11:41:56 +0200

    feat: add Suspense to image

--- src/features/team/components/cardPlayer/CardPlayer.tsx
+++ src/features/team/components/cardPlayer/CardPlayer.tsx
@@ -1,6 +1,7 @@
 import styles from "./cardPlayer.module.css"
 import Image from "next/image"
 import {MemberProps} from "@/features/users/type";
+import {Suspense} from "react";
 
 
 export default function CardPlayer({player} : {player : MemberProps}) {
@@ -9,9 +10,11 @@ export default function CardPlayer({player} : {player : MemberProps}) {
     return (
         <>
         <section  >
-            <figure>
-                <Image src={player.photo as string} alt={player.firstname} width={320} height={320} />
-            </figure>
+            <Suspense>
+                <figure>
+                    <Image src={player.photo as string} alt={player.firstname} width={320} height={320} />
+                </figure>
+            </Suspense>
             <article className={styles.info}>
                 <h2>{player.firstname}</h2>
                 <h2>{player.lastname}</h2>
