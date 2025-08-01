2de9bc9c9df97a2b4d8948c9b8af0f5da89b8c47
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-08-01 11:56:54 +0200

    feat: add metadata for each page

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -4,9 +4,14 @@ import NavBar from "@/components/layout/navigation/navBar/NavBar";
 import Footer from "@/components/layout/footer/Footer";
 import {readAllCategories} from "@/features/team/action";
 import {notFound} from "next/navigation";
+import type {Metadata} from "next";
 
 
-
+export const metadata : Metadata = {
+    title: "EFFCA | Entente Fons Fourmagnac Camburat Assier",
+    description: "Club de foot de l'Entente Fons Fourmagnac Camburat Assier",
+    icons : "/images/logo.webp",
+}
 
 
 export default async function LayoutClient({children}: Readonly<{children: ReactNode}>) {
--- src/app/(client)/login/page.tsx
+++ src/app/(client)/login/page.tsx
@@ -1,4 +1,11 @@
 import LoginForm from "@/features/auth/components/loginForm/LoginForm";
+import {Metadata} from "next";
+
+export const metadata : Metadata = {
+    title: "EFFCA | Login",
+    icons : "/images/logo.webp",
+}
+
 
 export default function LoginPage(){
 
